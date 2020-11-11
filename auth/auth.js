4ab574de20b8883bdcc169b2dacf22b92db28cbe
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-11 10:49:08 +0530

    feat: add a Passport middleware to handle user login

--- auth/auth.js
+++ auth/auth.js
@@ -20,3 +20,22 @@ passport.use(
         }
     )
 );
+
+passport.use(
+    'signup',
+    new localStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password'
+        },
+        async (email, password, done) => {
+            try {
+                const user = await UserModel.create({ email, password });
+
+                return done(null, user);
+            } catch (error) {
+                done(error);
+            }
+        }
+    )
+);
