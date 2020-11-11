997ec9edc63c0f08e01971ad3f8b3aa8174a0e3b
dinushchathurya <dinushchathurya21@gmail.com>
2020-11-11 10:47:49 +0530

    feat: add a Passport middleware to handle user registration

---
+++ auth/auth.js
@@ -0,0 +1,22 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const UserModel = require('../model/model');
+
+passport.use(
+    'signup',
+    new localStrategy(
+        {
+            usernameField: 'email',
+            passwordField: 'password'
+        },
+        async (email, password, done) => {
+            try {
+                const user = await UserModel.create({ email, password });
+
+                return done(null, user);
+            } catch (error) {
+                done(error);
+            }
+        }
+    )
+);
